62fd5aa9f82e40304dcc64bd5c81645bc996985f
Gaston Barboza <kuizengg@gmail.com>
2023-12-27 19:31:31 -0300

    Ejercicio 2, actividad 2

---
+++ Ejercicio2/Actividad7/main.js
@@ -0,0 +1,5 @@
+let tecla = prompt("coloque la letra y. Le diremos si la ha colocado correctamente.")
+
+if (tecla === "y" || tecla === "Y") {
+    alert("Ingresó la tecla correcta")
+} else alert("Ingresó la tecla incorrecta.")
