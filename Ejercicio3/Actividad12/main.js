a4d7deb3bc13f767a7c1ec1817e6a11642ca2a9e
Gaston Barboza <kuizengg@gmail.com>
2023-12-27 20:42:24 -0300

    Ejercicio3 actividad 2 resuelto.

---
+++ Ejercicio3/Actividad12/main.js
@@ -0,0 +1,6 @@
+let numeroSolicitado = parseFloat(prompt("Ingrese un numero."));
+let iteraciones = 1
+while(iteraciones <= numeroSolicitado && iteraciones < 6) {
+    alert(`vez numero ${iteraciones}: lado.`)
+    iteraciones++
+}
