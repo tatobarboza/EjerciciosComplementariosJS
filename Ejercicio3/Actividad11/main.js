75cf7e96b9417fd27660be390ec2f780ed20fe1d
Gaston Barboza <kuizengg@gmail.com>
2023-12-27 20:33:01 -0300

    Ejercicio 3 actividad 1 resuelto

---
+++ Ejercicio3/Actividad11/main.js
@@ -0,0 +1,6 @@
+let numeroIngresado = parseInt(prompt("Ingrese un numero"));
+let texto = prompt("Ingrese Un texto")
+
+for (let i = 1; i <= numeroIngresado;i++) {
+    alert(`texto ingresado: ${texto} || repetición numero: ${i}`)
+}
