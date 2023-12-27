a1582064b3a9c443b98a8630b8e962449edcbfd1
Gaston Barboza <kuizengg@gmail.com>
2023-12-27 19:19:03 -0300

    ejercicio2 actividad 1

---
+++ Ejercicio2/Actividad6/main.js
@@ -0,0 +1,21 @@
+let nom1 = "german"
+let nom2 = "agustin"
+let nom3 = "robert"
+let nom4 = "juan"
+let nom5 = "alcancio"
+
+const nombreIngresado = prompt("Ingrese un nombre a comparar.")
+
+if (nombreIngresado === nom1) {
+    alert("Fui yo")
+} else if (nombreIngresado === nom2){
+    alert("Fui yo")
+} else if (nombreIngresado === nom3) {
+    alert("Fui yo")
+} else if (nombreIngresado === nom4) {
+    alert("Fui yo")
+} else if (nombreIngresado === nom5) {
+    alert("Fui yo")
+} else alert("No fui yo");
+
+
